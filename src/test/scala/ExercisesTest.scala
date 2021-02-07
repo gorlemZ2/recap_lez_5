91335b6990bcba945c2b7cf5d84a6521e5679869
GorlemZ <andrea.alice.gorletta@gmial.com>
2021-02-07 21:25:06 +0100

    AVG exercise tests

--- src/test/scala/ExercisesTest.scala
+++ src/test/scala/ExercisesTest.scala
@@ -3,6 +3,9 @@ import java.lang.System.Logger
 import org.scalatest._
 import flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import collection.JavaConverters._
+
+import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 class ExercisesTest extends AnyFlatSpec with Matchers {
@@ -16,6 +19,23 @@ class ExercisesTest extends AnyFlatSpec with Matchers {
 
     Sums.firstV(x, y) shouldBe res
     Sums.secondV(x, y) shouldBe res
+
+  }
+
+  "AvgEx" should "be a cool homework" in {
+
+    val l = List(4,5,6,7,8,9,10)
+    val res = l.sum/l.length
+    val evenres= l.filter(_%2==0).sum/l.count(_ % 2 == 0)
+    val oddres= l.filter(_%2!=0).sum/l.count(_ % 2 != 0)
+
+
+    val ls: java.util.List[Integer] = ArrayBuffer[Integer](4,5,6,7,8,9,10).asJava
+
+
+    AvgEx.avg(ls) shouldBe res
+    AvgEx.evenAvg(ls) shouldBe evenres
+    AvgEx.oddAvg(ls) shouldBe oddres
 
   }
 
