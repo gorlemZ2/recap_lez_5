82be3496f83a0a2e4c067cc9f453a02c3bd4f7c6
GorlemZ <andrea.alice.gorletta@gmial.com>
2021-02-07 21:26:29 +0100

    ident

--- src/main/java/Sums.java
+++ src/main/java/Sums.java
@@ -2,51 +2,47 @@ import java.util.Scanner;
 
 public class Sums {
 
-	//Esprimete con le strutture di controllo fondamentali un algoritmo per il calcolo della somma di
-	//tutti i numeri interi compresi tra x e y, estremi inclusi, dove x e y sono due interi forniti dall’utente.
-	//Se ad esempio l’utente fornisce 3 e 8 il risultato dovr`a essere 33.
+    //Esprimete con le strutture di controllo fondamentali un algoritmo per il calcolo della somma di
+    //tutti i numeri interi compresi tra x e y, estremi inclusi, dove x e y sono due interi forniti dall’utente.
+    //Se ad esempio l’utente fornisce 3 e 8 il risultato dovr`a essere 33.
 
-	/**
-	 * Nella prima versione supponete
-	 * che l’utente fornisca i numeri in ordine non decrescente.
-	 */
+    /**
+     * Nella prima versione supponete
+     * che l’utente fornisca i numeri in ordine non decrescente.
+     */
 
-	public static int firstV(int x, int y) {
-		//qui la tua funzione
-		return x;
-	}
+    public static int firstV(int x, int y) {
+        //qui la tua funzione
+        return 0;
+    }
 
-	/**
-	 * Nella seconda versione supponete
-	 * che l’utente fornisca i numeri in ordine casuale.
-	 */
+    /**
+     * Nella seconda versione supponete
+     * che l’utente fornisca i numeri in ordine casuale.
+     */
 
-	public static int secondV(int x, int y) {
-		//qui la tua funzione
-		return x;
-	}
+    public static int secondV(int x, int y) {
+        //qui la tua funzione
+        return 0;
+    }
 
-	public static void main (String[] args) {
-	
+    public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int x = scan.nextInt();
 
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the first number: ");
-		int x = scan.nextInt();
+        System.out.print("Enter the second number: ");
+        int y = scan.nextInt();
 
-		System.out.print("Enter the second number: ");
-		int y = scan.nextInt();
+        // ricordati di chiudere lo scanner
+        scan.close();
 
-		// ricordati di chiudere lo scanner
-		scan.close();
+        int result = firstV(x, y);
 
+        // Displaying the number
+        System.out.println("The sum of all the numbers from " + x + " to " + y + " is: ");
+        System.out.println(result);
 
-		int result = firstV(x,y);
-
-
-		// Displaying the number
-		System.out.println("The sum of all the numbers from " +x+ " to " +y+ " is: ");
-		System.out.println(result);
-
-	}
+    }
 }
--- src/test/scala/ExercisesTest.scala
+++ src/test/scala/ExercisesTest.scala
@@ -15,7 +15,7 @@ class ExercisesTest extends AnyFlatSpec with Matchers {
 
     val x = Random.nextInt(100)
     val y = Random.nextInt(100)
-    val res = if(x<y) (x to y).toList.sum  else (y to x).toList.sum
+    val res = if (x < y) (x to y).toList.sum else (y to x).toList.sum
 
     Sums.firstV(x, y) shouldBe res
     Sums.secondV(x, y) shouldBe res
@@ -24,13 +24,13 @@ class ExercisesTest extends AnyFlatSpec with Matchers {
 
   "AvgEx" should "be a cool homework" in {
 
-    val l = List(4,5,6,7,8,9,10)
-    val res = l.sum/l.length
-    val evenres= l.filter(_%2==0).sum/l.count(_ % 2 == 0)
-    val oddres= l.filter(_%2!=0).sum/l.count(_ % 2 != 0)
+    val l = List(4, 5, 6, 7, 8, 9, 10)
+    val res = l.sum / l.length
+    val evenres = l.filter(_ % 2 == 0).sum / l.count(_ % 2 == 0)
+    val oddres = l.filter(_ % 2 != 0).sum / l.count(_ % 2 != 0)
 
 
-    val ls: java.util.List[Integer] = ArrayBuffer[Integer](4,5,6,7,8,9,10).asJava
+    val ls: java.util.List[Integer] = ArrayBuffer[Integer](4, 5, 6, 7, 8, 9, 10).asJava
 
 
     AvgEx.avg(ls) shouldBe res
