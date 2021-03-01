4779a3945b45d629ec62925c14d92a024cf3311d
GorlemZ <andrea.alice.gorletta@gmial.com>
2021-03-01 19:30:33 +0100

    templ

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 organization := "mohole.it"
 
-name := "Exercises Lez2"
+name := "Exercises Lez4"
 
 version := "1.0-SNAPSHOT"
 scalaVersion := "2.11.7"
--- src/test/scala/ExercisesTest.scala
+++ src/test/scala/ExercisesTest.scala
@@ -11,30 +11,7 @@ class ExercisesTest extends AnyFlatSpec with Matchers {
 
   "Sums" should "sum all numbers" in {
 
-    val x = Random.nextInt(100)
-    val y = Random.nextInt(100)
-    val res = if (x < y) (x to y).toList.sum else (y to x).toList.sum
-
-    Sums.firstV(x, y) shouldBe res
-    Sums.secondV(x, y) shouldBe res
-
   }
 
-  "AvgEx" should "be a cool homework" in {
-
-    val l = List(4, 5, 6, 7, 8, 9, 10)
-    val res = l.sum / l.length
-    val evenres = l.filter(_ % 2 == 0).sum / l.count(_ % 2 == 0)
-    val oddres = l.filter(_ % 2 != 0).sum / l.count(_ % 2 != 0)
-
-
-    val ls: java.util.List[Integer] = ArrayBuffer[Integer](4, 5, 6, 7, 8, 9, 10).asJava
-
-
-    AvgEx.avg(ls) shouldBe res
-    AvgEx.evenAvg(ls) shouldBe evenres
-    AvgEx.oddAvg(ls) shouldBe oddres
-
-  }
 
 }
